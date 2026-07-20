7572ffd07266b7d287444403913cd69475688bf2
khyatikumar <anuradhamaruvada@gmail.com>
2026-07-20 10:11:01 +0530

    testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,36 +1,28 @@
-# from fastapi import FastAPI
-
-# from backend.app.api.router import api_router
-# from fastapi.middleware.cors import CORSMiddleware
-# app = FastAPI(
-#     title="LifeTwin AI Backend",
-#     version="1.0.0",
-# )
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[
-#         "http://localhost:3000",
-#         "http://127.0.0.1:3000",
-#     ],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.include_router(api_router)
-
-
-# @app.get("/")
-# def root():
-#     return {
-#         "message": "LifeTwin AI Backend Running"
-#     } 
-
-
 from fastapi import FastAPI
 
-app = FastAPI()
+from backend.app.api.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI(
+    title="LifeTwin AI Backend",
+    version="1.0.0",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://lifetwin-1.onrender.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router)
+
 
 @app.get("/")
 def root():
-    return {"message": "Hello Render"}
+    return {
+        "message": "LifeTwin AI Backend Running"
+    }
